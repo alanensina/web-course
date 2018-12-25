f453350b68bf904feb4f1c068e5b02621091aa55
Alan Ensina <alanvinicius.ce@gmail.com>
2018-12-25 15:58:43 -0200

    Cleaning the mess

--- Fundamentos/usandoVARemLoop1.js
+++
@@ -1,6 +0,0 @@
-// Como o VAR não possui escopo de bloco, a variável criada dentro do for ela continua fora do escopo
-for (var i = 0; i < 10; i++) {
-    console.log(i)
-}
-
-console.log("i = ", i)
